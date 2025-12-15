4257f7183f281ad272e464bc8c6f100c5a6854fb
AdarshEtipakulu <balu897888@gmail.com>
2025-12-15 13:39:28 +0530

    Add files via upload

---
+++ Balu.java
@@ -0,0 +1,12 @@
+ class Balu{
+    static int add(){
+    int x;
+    x=100;
+     int y=3;
+    return x+y;
+ }
+ public static void main(String args[]){
+    int z=add();
+    System.out.println(z);
+ }
+}
