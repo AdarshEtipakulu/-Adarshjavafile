b5dc22f9b26fe015c9cf5f1b5e4459e223205096
AdarshEtipakulu <balu897888@gmail.com>
2025-12-17 07:33:03 +0530

    Create bankexmethod.java

---
+++ bankexmethod.java
@@ -0,0 +1,31 @@
+class Bank {
+
+    double amount;
+
+    // constructor
+    Bank(double amount) {
+        this.amount = amount;
+    }
+
+    void withdraw(double w) {
+        String msg = (w <= amount) ? "Withdrawal successful" : "Insufficient balance";
+        System.out.println(msg);
+
+        if (w <= amount)
+            amount = amount - w;
+    }
+
+    void deposit(double d) {
+        amount = amount + d;
+    }
+
+    public static void main(String[] args) {
+
+        Bank b = new Bank(10000);
+
+        b.withdraw(3000);
+        b.deposit(5000);
+
+        System.out.println("Final Balance = " + b.amount);
+    }
+}
