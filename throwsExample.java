c07546b7a4a84e745bc3a0ed6bfafc045b43392a
AdarshEtipakulu <balu897888@gmail.com>
2025-12-18 10:30:32 +0530

    Create throwsExample.java

---
+++ throwsExample.java
@@ -0,0 +1,18 @@
+class Demo {
+    void div() throws ArithmeticException {
+        int n1 = 20;
+        int n2 = 0;
+        System.out.println(n1 / n2);
+    }
+}
+
+public class Example {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        try {
+            d.div();
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Cannot divide by zero.");
+        }
+    }
+}
